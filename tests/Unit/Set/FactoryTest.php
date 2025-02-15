4077f2350aa315d451e70ecd4837dfc132a5ea39
David Smith <dasmith@zero-to-prod.com>
2025-02-15 09:26:30 -0500

    feat/factory Created `tests/Unit/Set/FactoryTest.php`.

---
+++ tests/Unit/Set/FactoryTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Unit\Set;
+
+use Tests\TestCase;
+
+class FactoryTest extends TestCase
+{
+    /**
+     * @test
+     *
+     * @see Factory
+     */
+    public function from(): void
+    {
+        $User = UserFactory::factory()
+            ->set(User::first_name, 'John')
+            ->set([User::last_name => 'Doe'])
+            ->set(function (array $context) {
+                return [User::surname => $context[User::last_name]];
+            })
+            ->set('address.postal_code', '46789')
+            ->make();
+
+        self::assertEquals('John', $User[User::first_name]);
+        self::assertEquals('Doe', $User[User::last_name]);
+        self::assertEquals('Doe', $User[User::surname]);
+        self::assertEquals('46789', $User[User::address]['postal_code']);
+    }
+}
