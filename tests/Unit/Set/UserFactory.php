6cf11b31317b406317e5af28bd930ebb04556be4
David Smith <dasmith@zero-to-prod.com>
2025-02-15 09:26:30 -0500

    feat/factory Created `tests/Unit/Set/UserFactory.php`.

---
+++ tests/Unit/Set/UserFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit\Set;
+
+use Zerotoprod\Factory\Factory;
+
+class UserFactory
+{
+    use Factory;
+
+    protected function definition(): array
+    {
+        return [
+            User::first_name => 'John',
+            User::last_name => 'N/A',
+            User::address => [
+                'postal_code' => 'a',
+            ],
+        ];
+    }
+
+}
